5e9c76256f566c3d9d1fecd03db446ab76a5b995
ParisStacy <parispluto@gmail.com>
2020-04-24 14:05:54 -0700

    Added Journal Script, Fixed Player Controller Bugs, Added Brush Texture (Obsolete)

---
+++ Assets/Scripts/Paris-Scripts/JournalScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalScript : MonoBehaviour
+{
+     void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
